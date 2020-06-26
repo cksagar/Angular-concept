7b3eda4a77ca0f2e439316c18b1272fe809f99cb
chetan <cksagar7@gmail.com>
2020-06-26 13:44:08 +0530

    same commit

---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+    @Component({
+        selector:'courses',
+        template:'<h2>{{getTitle()}}</h2>'    
+    })
+    export class CoursesComponent{
+        title = "now my name is cksagar"
+        getTitle(){
+       return this.title
+    }
+    }
